50fd0b6363213abf38c6edb730a20919fb65a53b
Joseph <jfghanimah@gmail.com>
2021-02-19 08:18:59 +0000

    added options to minimize webdriver footprint

--- app/chegg_scraper.py
+++ app/chegg_scraper.py
@@ -1,21 +1,38 @@
 import os
 import pickle
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 # Takes in the URL of a chegg page logs in and grabs the right material
 def get_chegg_images(URL, user=None, password=None):
-
+    # Setup Options before driver
     chrome_options = Options()
     chrome_options.headless = True
-    chrome_options.add_argument('window-size=1920,1080')
-    chrome_options.add_argument('--user-data-dir=/home/joseph/g3-bot/app/user')
+    
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument("--disable-blink-features")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--disable-web-security")
+    #chrome_options.add_argument("--proxy-server=localhost:8080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--user-data-dir=/home/joseph/g3-bot/app/user")
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36")
+
     driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
     driver.get(url)
     input("finish")
     driver.quit()
+
     return None
 
 url = "https://www.chegg.com/homework-help/questions-and-answers/consider-circuit--use-node-voltage-method-obtain-node-voltages-v1-v2-v3-use-linsolve-funct-q55827065"
-url = "https://www.google.com"
+url = "https://www.josephghanimah.com"
+url = "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html"
+
+
+get_chegg_images(url)
